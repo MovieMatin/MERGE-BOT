60f9c905b93797007caca2f4e9d2acaa7dd4d6a2
MovieMatin <125181425+MovieMatin@users.noreply.github.com>
2023-03-01 16:48:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TELEGRAM_API = 15236804
     OWNER = "1963114305"
     OWNER_USERNAME = "Md_Matin_Ashraf"
-    PASSWORD = "QAZ1qaz@QAZ1qaz@"
+    PASSWORD = "ThanksMatin"
     DATABASE_URL = "mongodb+srv://MdMatin:x7bdggKJ9zb9JSK@cluster0.89bzvjn.mongodb.net/?retryWrites=true&w=majority"
     LOGCHANNEL = "-1001812984233"  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
