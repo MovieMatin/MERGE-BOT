dcae137532b19cb0b156c2a11026c4b22adb6c4b
MovieMatin <125181425+MovieMatin@users.noreply.github.com>
2023-03-15 09:47:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ class Config(object):
     LOGCHANNEL = "-1001812984233"  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
     USER_SESSION_STRING = "BQA1n8pqOo92YxshhlSjhsJ2zx72vbNOBlGPPBGaA5xvmmBi63aew6HC093zNeTmw6BzhDAe1wah4p0lGy_InGrr70JhW9Ru2ySH446JbSP1cQGp-55IqFMq1iUsD1l2YffOF3ofsceTS9_nt764RNVbvPFcBbmHde78YdeCNpYowOzXcVpCyuuLjxQ-UCNAJVOKhGZ1VScG53M6_K9D8LL6F1be00JBwTKUb4kkCEL_CYzgSKPSWk6-e3D9oOfqPI7dWFtWmywjwcaxVhwl-3b1ApNq95st-Next4uZ8hzzhwr3ddqmu5-uuqGfniNnbkD4o1JWgMbUgawyDKRv4umkAAAAAHUCv0EA"
-    IS_PREMIUM = "True"
+    IS_PREMIUM = False
     MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
