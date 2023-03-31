10b955f9195d4cc83714978d22bbd4c15062e738
MovieMatin <125181425+MovieMatin@users.noreply.github.com>
2023-04-01 01:11:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class Config(object):
     DATABASE_URL = "mongodb+srv://MdMatin:x7bdggKJ9zb9JSK@cluster0.89bzvjn.mongodb.net/?retryWrites=true&w=majority"
     LOGCHANNEL = "-1001812984233"  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
-    USER_SESSION_STRING = "BQDofsQAt2MgIh8bjpwOGKQ_EBzr9eDX1USc6rRrFH-4lNb99v5JMg-XwYQz24fPe9YMIbfgbMVtmkhgiGIhwP1zEaLpU2_GX18XE_nsaAE7Utd3Sl4tfO_MkQodyQ3bQiEPsavenLKAknj8vPrpRB0xQ4TlShY8kqx1TpVHxj-NXuXxVGqkbIHtPa3_HiUK98atd9mgbL_YG1XCas_r8Sz_nPytJYJqF3YCttD-8viOagB1YadW2zTtIs7iF--Hh53Sc7fMbkfw8fWk0P-uGvVLzfWLv3nEhLUF_em49_K_4weY45TsCFXB3mHdzTVxTqn0fkupABwfoNDkRU0cDKx9J0wAMwAAAAB1Ar9BAA"
+    USER_SESSION_STRING = "BQDofsQAF9IAiY040H864RTKMvhRqjyMdKzmWT-UbLcDRdy1KIiZv_bDZYBd5wawIgSaIUNdaiRvq4sWbCGDXN6YvDBT59GjSzaGaNn0TsdzEqQfWkk68FFcStkn1MKfxt5CiLqmAfpMUQwrcCLi3aK4Fby2wIjSUbgjKu6iaZX2k7glP-2MUXwM0GiSe7XrRLWeoOPknFroSUfE_kNbbZv9mveGgmSeCke7RCYZMb8jpOE06GOetgs3GF6zcq3s-kR_ywgi27gEAHs2FP8DVUe7Ol0cSxPu3lYxy_xjQQ8BpUXNJOEHAJ5omJjpSsYTYEr7h7Esxyr8E7jHBZDHlzlCrdIPSwAAAAB1Ar9BAA"
     IS_PREMIUM = "True"
     MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
